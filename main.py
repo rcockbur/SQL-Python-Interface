ecabaf74df7b75d3a5ca0932c722d7b959da54ac
rcockbur <rcockbur@cs.ualberta.ca>
2014-10-28 14:18:28 -0600

    Ross's first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+print('')
+print('Main Menu')
+print('1. Prescribe a Test')
+print('2. Enter Test Results')
+print('3. Update Patient Information')
+print('4. View Patient History') #returns all test rest results 
+print('5. View Doctor Activity')
+print('6. View High Risk Patients')
+i = input()
+
