2a0ff23cd2d25e7cb623a38f7fde927c9325ee0e
rcockbur <rcockbur@cs.ualberta.ca>
2014-10-28 14:52:50 -0600

    finished menu skeleton code

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 import sys
-
-print('')
-print('Main Menu')
-print('1. Prescribe a Test')
-print('2. Enter Test Results')
-print('3. Update Patient Information')
-print('4. View Patient History') #returns all test rest results 
-print('5. View Doctor Activity')
-print('6. View High Risk Patients')
-i = input()
+while(1):
+	print('')
+	print('Main Menu')
+	print('1. Prescribe a Test')
+	print('2. Enter Test Results')
+	print('3. Update Patient Information')
+	print('4. View Patient History') #returns all test rest results 
+	print('5. View Doctor Activity')
+	print('6. View High Risk Patients')
+	i = input('Select Option 1-6:')
+	while(1):
+		if i == '1':
+			#doSomething
+			print(i)
+			break;
+		elif i == '2':
+			#doSomething
+			print(i)
+			break;
+		elif i == '3':
+			#doSomething
+			print(i)
+			break;
+		elif i == '4':
+			#doSomething
+			print(i)
+			break;
+		elif i == '5':
+			#doSomething
+			print(i)
+			break;
+		elif i == '6':
+			#doSomething
+			print(i)
+			break;
+		else: 
+			i = input('Invalid Entry, Select Option 1-6:')
 
