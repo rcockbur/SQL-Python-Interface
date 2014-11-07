d22fbc5cc73c3b5559041d2e29502d13f81357c1
Sarah Van Belleghem <vanbelle@cs.ualberta.ca>
2014-11-07 16:47:24 -0700

    first and second search modules seem to work

--- SearchEngine.py
+++ SearchEngine.py
@@ -28,6 +28,31 @@ def searchPatient():
 		print('')	
 		
 def searchDoctor():
+	start_date = input('Enter the starting date(YYYY-MM-DD): ')
+	end_date = input('Enter the end date(YYYY-MM-DD): ')
+	sdate = start_date.split('-')
+	edate = end_date.split('-')
+	curs.execute("SELECT distinct employee_no name FROM doctor")
+	doctors = curs.fetchall()
+	doctor_nos = [i[0] for i in doctors] 	
+	while True:
+		employee_no = int(input("Enter doctor's employee number: "))
+		if employee_no in doctor_nos:
+			break
+		cont = input("That employee number is not on file, would you like to try again (Y/N): ")          
+		if cont == 'N':
+			return	
+	curs.execute("SELECT t.patient_no, p.name, tt.test_name, t.prescribe_date FROM patient p, test_record t, test_type tt WHERE t.type_id = tt.type_id AND p.health_care_no = t.patient_no AND t.employee_no = %d AND t.prescribe_date >= to_date('%d-%d-%d', 'YYYY-MM-DD') AND t.prescribe_date <= to_date('%d-%d-%d', 'YYYY-MM-DD')" %(employee_no,int(sdate[0]),int(sdate[1]), int(sdate[2]), int(edate[0]), int(edate[1]), int(edate[2])))
+	test_list = curs.fetchall()
+	print(' ')
+	if test_list == None:
+		print("that doctor has not prescribed any tests")
+	for row in test_list:
+		print('Health Care Number: ' +str(row[0]))
+		print('Name: ' + row[1])
+		print('Test Name: ' + row[2])
+		print('Prescribe Date: ' + str(row[3].year) +'-'+ str(row[3].month) +'-'+ str(row[3].day))
+		print('')	
 	pass 
 
 def alarmingAge():
