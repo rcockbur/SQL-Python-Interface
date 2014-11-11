b1e8c1a53b399f65b43ef3d92069c21d37b5a621
Megan Sumner <msumner@cs.ualberta.ca>
2014-11-10 18:26:26 -0700

    FINISHED ALL THE THINGS

--- MedicalTest.py
+++ MedicalTest.py
@@ -2,7 +2,7 @@
 
 import random
 import cx_Oracle
-
+import gp1_main
 
 con = cx_Oracle.connect("vanbelle/c1234567@gwynne.cs.ualberta.ca:1521/CRS")
 cur = con.cursor()
@@ -11,7 +11,7 @@ cur = con.cursor()
 patient = "SELECT patient_no, test_name FROM test_record tr, test_type tt WHERE tr.type_id = tt.type_id AND result IS NULL"
 cur.execute(patient)
 rows = cur.fetchall()
-
+print(rows)
 again = True
 while again:
 	patient_no = int(input('Enter the health care number of the patient: '))
@@ -20,7 +20,6 @@ while again:
 
 	valid = False
 	for i in rows:
-		print(i)
 		if i == combined:
 			valid = True
 
@@ -32,13 +31,19 @@ while again:
 	if valid:
 		again = False
 
-lab_name = input('Enter your lab name: ')
-test_id = input('Enter the Id of the test ')
-test_result = input('Enter the result of the test: ')
+up = 'UPDATE test_record SET '
+medical_lab ='medical_lab= ' + "'" + input('Enter your lab name: ') + "'" +  ', '
+result = 'result = ' + "'" + input('Enter the result of the test: ') + "'" + ','
+test_date = 'test_date= ' + 'to_date(' + "'" + input('Enter the date the test was constructed: (YYYY-MM-DD)') + "'" + ',' +"'"+ 'YYYY-MM-DD' + "'" + ')'
+fr = ' WHERE patient_no =' + str(patient_no) +  ' and result IS NULL'
+command = up + medical_lab + result + test_date + fr
+print(command)
 
 
-cur.execute("UPDATE test_record SET result = :result WHERE test_id = :test_id", {'result' : str(test_result), 'test_id' :int(test_id)})
+
+cur.execute(command)
 
 
 con.commit()
+cur.close()
 con.close()
