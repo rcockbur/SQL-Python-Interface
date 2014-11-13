66886daf8ad3125ed6d2b6a60ed870baec877001
Megan Sumner <msumner@cs.ualberta.ca>
2014-11-12 20:09:28 -0700

    added input for username and password

--- MedicalTest.py
+++ MedicalTest.py
@@ -1,19 +1,18 @@
 #Medical Test module
 
-
 import cx_Oracle
 
 class MedicalTest(object):
 	def __init__(self):
+		username = input("Enter your Username: ")
+		password = input("Enter your Password: ")
 		super(MedicalTest, self).__init__()
-		con = cx_Oracle.connect("vanbelle/c1234567@gwynne.cs.ualberta.ca:1521/CRS")
+		con = cx_Oracle.connect(username +"/"+password+"@gwynne.cs.ualberta.ca:1521/CRS")
 		cur = con.cursor()
-
 		#Get a list of the patient number and test type for each result that has not been completed.
 		patient = "SELECT patient_no, test_name FROM test_record tr, test_type tt WHERE tr.type_id = tt.type_id AND result IS NULL"
 		cur.execute(patient)
 		rows = cur.fetchall()
-		print(rows)
 		again = True
 		while again:
 			patient_no = int(input('Enter the health care number of the patient: '))
@@ -39,12 +38,9 @@ class MedicalTest(object):
 		test_date = 'test_date= ' + 'to_date(' + "'" + input('Enter the date the test was constructed: (YYYY-MM-DD)') + "'" + ',' +"'"+ 'YYYY-MM-DD' + "'" + ')'
 		fr = ' WHERE patient_no =' + str(patient_no) +  ' and result IS NULL'
 		command = up + medical_lab + result + test_date + fr
-		print(command)
-
-
 
 		cur.execute(command)
-
+		print("Medical test updated")
 
 		con.commit()
 		cur.close()
